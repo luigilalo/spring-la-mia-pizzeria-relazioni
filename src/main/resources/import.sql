b8056d47df8dbbf1a21f1b4cff530baac2331f17
luigilalo <luigilaloe1@gmail.com>
2024-01-09 16:36:04 +0100

    Working

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO db_pizzeria.pizza
-(Nome, Descrizione, Foto, Prezzo)
-VALUES('Margherita', 'con Mozzarella', 'https://www.finedininglovers.it/sites/g/files/xknfdk1106/files/styles/recipes_1200_800/public/fdl_content_import_it/margherita-50kalo.jpg.webp?itok=QlO8_AHv', 5.50);
-INSERT INTO db_pizzeria.pizza
-(Nome, Descrizione, Foto, Prezzo)
-VALUES('Bianca', 'con pomodorini e mozzarella', 'https://blog.giallozafferano.it/lericettesvelate/wp-content/uploads/2020/03/BD5B56E8-2911-4B0C-9D45-0A0DF304FE9F.jpeg', 7);
