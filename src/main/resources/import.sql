3d009014865cfc435c8ba2cd6999c641967d046a
luigilalo <luigilaloe1@gmail.com>
2024-01-09 17:20:14 +0100

    Final Commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO db_pizzeria.pizza (descrizione,foto,nome,prezzo) VALUES
+	 ('con pomodorini e mozzarella','https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/CAB67512-5E27-40A1-8EBF-3F0C1A6E4DF1/Derivates/180245b0-b38b-4cac-82a2-7e2ed9d8aa62.jpg','Bianca',7.0),
+	 ('con Mozzarella','https://upload.wikimedia.org/wikipedia/commons/c/c8/Pizza_Margherita_stu_spivack.jpg','Margherita',5.5);
