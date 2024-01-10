7fd2656e56e96a5c321d26a9c2503f69e4940804
luigilalo <luigilaloe1@gmail.com>
2024-01-10 16:45:04 +0100

    Final Commit

--- src/main/java/org/learning/springlibrary/controller/PizzaController.java
+++ src/main/java/org/learning/springlibrary/controller/PizzaController.java
@@ -1,8 +1,12 @@
 package org.learning.springlibrary.controller;
 
+import jakarta.validation.Valid;
 import org.learning.springlibrary.model.Pizza;
+import org.learning.springlibrary.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,14 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/crea")
+
 public class PizzaController {
+    @Autowired
+    private PizzaRepository pizzaRepository;
     @GetMapping
     public String formPizza (Model model) {
         model.addAttribute("pizza", new Pizza());
         return "crea";
     }
     @PostMapping("/creaPizza")
-    public String creaPizza(@ModelAttribute Pizza pizza, Model model) {
-        return "redirect:/home";
+    public String creaPizza(@Valid @ModelAttribute("pizza") Pizza pizza, Model model, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/crea";
+        }
+        Pizza savedPizza = pizzaRepository.save(pizza);
+        return "redirect:/" + pizza.getId();
     }
 }
